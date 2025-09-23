b8b6768b95c18d606b849015fccc913a4edf79e6
GioTFG <168219065+GioTFG@users.noreply.github.com>
2025-09-23 23:35:53 +0200

    Svolto l'esercizio "The Bridge of Death".

---
+++ The Bridge of Death/bridge.py
@@ -0,0 +1,8 @@
+risposta1 = input("What is your name? ")
+risposta2 = input("What is your quest? ")
+risposta3 = input("What is your favourite colour? ")
+
+if risposta1 == "Lancelot" and risposta2 == "Holy Grail" and risposta3 == "Blue":
+    print("Right, off you go.")
+else:
+    print("Down into the Gorge of Eternal Peril!")
